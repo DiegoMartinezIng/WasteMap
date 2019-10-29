43a8e10df45c94668e637f03b0acb4a3b41d42d4
Katakusuz <diegoalejandro19982010@hotmail.com>
2019-10-29 00:36:23 -0500

    Lectura de base de datos json y ubicacion de puntos con nombre de los mismos

--- app/src/main/java/waste/map/Mapa.java
+++ app/src/main/java/waste/map/Mapa.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,17 +19,21 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class Mapa extends FragmentActivity implements OnMapReadyCallback {
-    ArrayList<String> arregloJson;
+    String[] arregloJson;
     private GoogleMap mMap;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
-        arregloJson = new ArrayList<>();
+        arregloJson = new String[2];
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -36,6 +41,7 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         db.collection("PuntosAcopio")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -43,19 +49,26 @@ public class Mapa extends FragmentActivity implements OnMapReadyCallback {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                
+                                 String Cadena = document.getData().get("Puntos").toString();
+                                try {
+                                    JSONObject json = new JSONObject(Cadena);
+                                    JSONArray arreglo = json.getJSONArray("Puntos");
+                                    for (int i = 0;i<arreglo.length();i++){
+                                        arregloJson = arreglo.getJSONObject(i).get("Ubicacion").toString().split(",");
+                                        Double coordenadaX = Double.valueOf(arregloJson[0].substring(1));
+                                        Double coordenadaY = Double.valueOf(arregloJson[1].substring(0,arregloJson[1].length()-1));
+                                        String nombrePuntero = arreglo.getJSONObject(i).getString("Nombre Punto de Recolección");
+                                        mMap.addMarker(new MarkerOptions().position(new LatLng(coordenadaX,coordenadaY)).title(nombrePuntero));
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
                         } else {
 
                         }
                     }
                 });
-        mMap = googleMap;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-       //Toast.makeText(this, arregloJson.get(0), Toast.LENGTH_SHORT).show();
     }
-
 }
