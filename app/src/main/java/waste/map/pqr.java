7ab5f4b763cc8603fd54feaed8cecfadc13d5818
almartinez3 <33075650+almartinez3@users.noreply.github.com>
2019-11-03 16:21:33 -0500

    se crea activity de pqr

---
+++ app/src/main/java/waste/map/pqr.java
@@ -0,0 +1,14 @@
+package waste.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pqr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pqr);
+    }
+}
